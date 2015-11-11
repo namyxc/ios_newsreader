b155e3d9fb2c171efa2a4681cb84cd019b85351e
rentit <rentit@rentits-MacBook-Pro-Xcode-6.local>
2015-11-11 14:54:59 +0100

    cached image

---
+++ NewsReader/ImageHandler.h
@@ -0,0 +1,13 @@
+//
+//  ImageHandler.h
+//  NewsReader
+//
+//  Created by rentit on 2015. 11. 11..
+//  Copyright © 2015. rentit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageHandler : NSObject
+
+@end
---
+++ NewsReader/ImageHandler.m
@@ -0,0 +1,13 @@
+//
+//  ImageHandler.m
+//  NewsReader
+//
+//  Created by rentit on 2015. 11. 11..
+//  Copyright © 2015. rentit. All rights reserved.
+//
+
+#import "ImageHandler.h"
+
+@implementation ImageHandler
+
+@end
